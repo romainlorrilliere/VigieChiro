4201b7cb3ef1c6dd7665167b5052f53a21b02dcb
Romain Lorrilliere <romainlorrilliere@gmail.com>
2019-08-01 17:26:21 +0200

    Add some function heads and comments

--- script_trend.r
+++ script_trend.r
@@ -2,6 +2,23 @@
 ####   Script de prepartation des data et d'analyse des tendances
 ##############################################
 
+
+## -----------------------------------------------------------------------------------------------------------
+
+## example:
+
+## d50 <- prepa_data(id="DataRP_SpTron_50",d="data/DataRP_SpTron_50.csv",aggregate_site=TRUE,add_abs=TRUE)
+## d90 <- prepa_data(id="DataRP_SpTron_90",d="data/DataRP_SpTron_90.csv",aggregate_site=TRUE,add_abs=TRUE)
+## main.glm()
+
+## -----------------------------------------------------------------------------------------------------------
+
+
+
+
+## the packages
+
+
 vecPackage=c("reshape2","data.table","lubridate","ggplot2","maps","readr","tidyr","arm","scales","glmmTMB","plyr","beepr")
 ip <- installed.packages()[,1]
 
@@ -9,7 +26,7 @@ for(p in vecPackage)
     if (!(p %in% ip))
         install.packages(pkgs=p,repos = "http://cran.univ-paris1.fr/",dependencies=TRUE)
 
-
+### These pathes could don't work
 source("../Vigie-Chiro_scripts/functions/GLMs/f_Sp_GLM_short.R")
 source("../Vigie-Chiro_scripts/functions/f_Coord_Bioclim.R")
 
@@ -39,7 +56,7 @@ assign("last.warning", NULL, envir = baseenv())
 ##' @param file path file
 ##' @param sep vector of separator tested default c("\t",";",",")
 ##' @param dec vector of decimal tested  default c(".",",")
-##' @param as.tibble logical to get a tibble format, default TRUE
+##' @param as_data.frame logical TRUE to get a data.frame
 ##' @param print_head logical to print to screen the head of data
 ##' @param max_col_head the maximal number of column if the head of data is printed
 ##' @param print_summary logical to print to screen the summary of the data
@@ -135,7 +152,14 @@ my_read_delim <- function(file,sep=c("\t",";",","),dec=c(".",","),as_data.frame=
     return(d)
 }
 
-
+##' .. content for \description{} (no empty lines) ..
+##' a simple function to print more easily the head of a dataset
+##' .. content for \details{} ..
+##' @title my_head
+##' @param d
+##' @param nbcol
+##' @return NULL
+##' @author Romain Lorrilliere
 my_head <- function(d,nbcol=NULL) {
     d <- as.data.frame(d)
     print(dim(d))
@@ -143,7 +167,14 @@ my_head <- function(d,nbcol=NULL) {
 }
 
 
-
+##' .. content for \description{} (no empty lines) ..
+##' a simple function to print a more parametrable summary
+##' .. content for \details{} ..
+##' @title my_summary
+##' @param d
+##' @param nbcol
+##' @return NULL
+##' @author Romain Lorrilliere
 my_summary <- function(d,nbcol=NULL) {
     d <- as.data.frame(d)
     print(dim(d))
@@ -152,7 +183,7 @@ my_summary <- function(d,nbcol=NULL) {
 
 
 ##' .. content for \description{} (no empty lines) ..
-##'
+##' In the case of presence only dataset, this function add the absence (0) for each species on the sites where are abscente.
 ##' .. content for \details{} ..
 ##' @title Add absence in data set of observation
 ##' @param d data,
@@ -161,7 +192,7 @@ my_summary <- function(d,nbcol=NULL) {
 ##' @param col_value vectors of colmuns that will be update with absence, defaut NULL, if null all columns not present in col_gr and col_sp will be update with absence
 ##' @param dall if there are not all sample in d you can import a table with all sample with the column of col_gr, default NULL
 ##' @return update of d (data.frame) with 0 in the col_value(s) when species are absente
-##' @author
+##' @author Romain Lorrilliere
 add_abs <- function(d,col_gr=NULL,col_sp=NULL,col_value=NULL,col_info_gr=NULL,dall=NULL,fig=FALSE,rep="output",id=NULL) {
     ##     col_value = c("nb_contacts","temps_enr"); col_sp = "col_sp"; col_gr = c("col_sample","expansion_direct")
 
@@ -306,37 +337,39 @@ add_abs <- function(d,col_gr=NULL,col_sp=NULL,col_value=NULL,col_info_gr=NULL,da
 
 
 ##' .. content for \description{} (no empty lines) ..
-##'
+##' prepa_data() is the function to prepare the dataset before trend assement. Almost all parameters have a default value.
 ##' .. content for \details{} ..
-##' @title
-##' @param id
-##' @param d
-##' @param dpart
-##' @param dsite
-##' @param dSR
-##' @param output
-##' @param col_sample
-##' @param col_sp
-##' @param col_date
-##' @param col_site
-##' @param col_IndiceDurPip
-##' @param col_IndiceProbPip
-##' @param col_SampleRate
-##' @param col_tron
-##' @param col_nbcontact
-##' @param col_temps
-##' @param seuilProbPip
+##' @title prepa_data()
+##' @param id to identify the output
+##' @param d the data or path to get it
+##' @param dpart data of participation if this information are not in "d", dpart should be a data.frame or a path to get it
+##' @param dsite  data of sites if this information are not in "d", dpart should be a data.frame or a path to get it
+##' @param dSR  data of sample ratio if this information are not in "d", dpart should be a data.frame or a path to get it
+##' @param output boolean TRUE if you want to get output directly in the console
+##' @param col_sample name of the sample column
+##' @param col_sp name of the species column
+##' @param col_date name of the date column
+##' @param col_site name of the site column
+##' @param col_IndiceDurPip name of the indice of Pipistrella duration column
+##' @param col_IndiceProbPip name of the Pipistrella inditification probability column
+##' @param col_SampleRate  name of sample rate column
+##' @param col_tron name of tronçon or point column
+##' @param col_nbcontact name of number of contact column
+##' @param col_temps name of temps column
+##' @param seuilProbPip vector of the two value of confidence threshold, first value for direct and second of expansion.
 ##' @param seuilInfDurPipDirect
-##' @param seuilSR
-##' @param col_sp
-##' @param seuilSR_inf
-##' @param seuilSR_sup
-##' @param list_sample_cat
-##' @param aggregate_site
-##' @param add_absence
-##' @param list_sp
-##' @param first_columns
-##' @return
+##' @param seuilSupDurPipExp
+##' @param seuilSR data.frame of all sample rate thersold  tibble(expansion_direct=c("exp","direct","direct"),col_sp=c(NA,NA,"Nycnoc"),seuilSR_inf=c(441000,96000,44100),seuilSR_sup=c(2000000,384000,384000))
+##' @param list_sample_cat vector of sample categories used
+##' @param aggregate_site boolean TRUE if data aggregated at the site level
+##' @param add_absence boolean TRUE to add absence
+##' @param list_sp vector of species
+##' @param aggregationSp list of vectors for species aggregation
+##' @param first_columns vector for order of first columun in the output dataset
+##' @param only_first_columns boolean if TRUE only the first columns are in the output dataset
+##' @param excluded_columns vector of columns that will be excluded directly afeter the importation.
+##' @return data.frame formated to assess the species trends
+##'
 ##' @author Romain Lorrilliere
 prepa_data <- function(id="DataRP_SpTron_90",
                        d="data/DataRP_SpTron_90.csv",dpart="data/p_export.csv",dsite ="data/sites_localites.txt",dSR="data/SRmed.csv",
@@ -383,11 +416,7 @@ prepa_data <- function(id="DataRP_SpTron_90",
         toutCol <- setdiff(colnames(d),col_nbcontact)
         formulaAggregate <- as.formula(paste(col_nbcontact," ~ ", paste(toutCol,collapse=" + ")))
         d <- aggregate(formulaAggregate,d,sum)
-
     }
-
-
-
 
 
     if(!is.null(dpart)) {
@@ -851,78 +880,6 @@ prepa_data <- function(id="DataRP_SpTron_90",
 
 }
 
-historic_tron <- function(d,id=NULL) {
-
-
-    d <- subset(d,nb_contacts > 0)
-    d_first_year <- aggregate(year~num_site_txt,d,min)
-    colnames(d_first_year)[2] <- "first_year"
-    d_last_year <- aggregate(year~num_site_txt,d,max)
-    colnames(d_last_year)[2] <- "last_year"
-
-    d <- inner_join(d,d_first_year)
-    d <- inner_join(d,d_last_year)
-
-
-    dunique <- unique(d %>% select(c("num_site_txt","year","Tron","sample_cat")))
-    ## dd <- add_abs(dunique,col_gr=c("num_site_txt","year","sample_cat"),col_sp="Tron",col_value="survey")
-
-    dtron <- aggregate(year~num_site_txt+sample_cat+Tron,dunique,length)
-    colnames(dtron)[4] <- "nb_survey"
-
-    dmaxYear <- aggregate(nb_survey~num_site_txt+sample_cat,dtron,max)
-    colnames(dmaxYear)[3] <- "nbYear_ef"
-
-    dtron <- inner_join(dtron,dmaxYear)
-
-    dunique <- unique(dunique %>% select(-one_of(c("year"))))
-
-    dunique <- inner_join(dunique,d_first_year)
-    dunique <- inner_join(dunique,d_last_year)
-
-
-    dd <- inner_join(dtron,dunique)
-    dd$nbYear_th<- dd$last_year - dd$first_year + 1
-
-    dd$pro_th <- dd$nb_survey / dd$nbYear_th
-    dd$pro_ef <- dd$nb_survey / dd$nbYear_ef
-
-    dsite <- unique(dd %>% select(c("num_site_txt","sample_cat","first_year","nbYear_ef","nbYear_th")))
-
-    dsite$namesite <- paste(dsite$first_year," - ",dsite$num_site_txt," (",dsite$nbYear_ef,")",sep="")
-
-    dsite <- dsite %>% arrange(first_year,namesite)
-
-    dsite$numsite <- 1:nrow(dsite)
-    dsite$numsite <- sprintf("%03d", dsite$numsite)
-
-    dsite$namesite <- paste(dsite$first_year," - ",dsite$numsite," (",dsite$nbYear_ef,")",sep="")
-
-
-    dd <- inner_join(dd,dsite)
-
-    dd <- dd %>% arrange(num_site_txt,Tron)
-
-
-    gg <- ggplot(data=subset(dd,nbYear_ef >1),aes(x=num_site_txt,y=Tron,colour=pro_ef,size=nbYear_ef)) + facet_wrap(.~sample_cat,scales="free",nrow=2)
-    gg <- gg + geom_point()
-    gg <- gg + scale_colour_gradient2(low="#a50026",mid = "#fee08b",high="#063402",midpoint = .75)
-    gg <- gg + theme(axis.text.x = element_text(angle = 90, hjust = 1,vjust=.5,size=8))
-    gg <- gg + labs(x="Les sites",y="Les tronçons ou points",title=paste("Qualité de suivis -",id),size="Nombre\n d'années\n de suivie\n du site",colour="Proportion\n des années\n suivis\n du tronçons")
-    ggfile <- paste("output/surveyTroncon",id,".png",sep="")
-    cat("\n  -> [PNG]",ggfile,"\n")
-    ggsave(ggfile,gg,width=18,height=8)
-
-
-
-    filecsv <- paste("output/data_survey_troncon.csv",sep="")
-    cat("\n   --> [CSV]",filecsv)
-    write.table(d,filecsv,sep="\t",dec=".",row.names=FALSE)
-    cat("   DONE !\n")
-
-
-
-}
 
 
 
@@ -943,76 +900,36 @@ affectCatEBCC <- function(trend,pVal,ICinf,ICsup){
 
 
 
-summary_sp <- function(d,id=NULL,save=c("length","wide"),output="") {
-
-    ds <- aggregate(nb_contacts_strict~num_site_txt+ year + expansion_direct + espece,d,max,na.rm=TRUE)
-    ds$occ_strict <- ifelse(ds$nb_contacts_strict>0,1,0)
-
-    df <- aggregate(nb_contacts_flexible~num_site_txt+ year + expansion_direct + espece,d,max,na.rm=TRUE)
-    df$occ_flexible <- ifelse(df$nb_contacts_flexible>0,1,0)
-
-    d <- full_join(ds,df)
-
-
-    dagg <- aggregate(cbind(d$occ_strict,d$occ_flexible)~year + expansion_direct + espece, d, sum,na.rm=TRUE)
-    colnames(dagg)[4:5] <- c("cc_strict","occ_flexible")
-
-
-    dtot <-  aggregate(cbind(d$occ_strict,d$occ_flexible)~ espece, d, sum,na.rm=TRUE)
-    colnames(dtot)[2:3] <- c("tot_strict","tot_flexible")
-
-    dtot$name <- paste(dtot$espece," (",dtot$tot_strict,")",sep="")
-
-    dagg <- inner_join(dagg,dtot)
-
-
-    dagg$name <- factor(dagg$name, levels = dtot$name[order(dtot$tot_strict,decreasing=TRUE)])
-
-    gg <- ggplot(d=subset(dagg,tot_strict>10),aes(x=year,y=cc_strict,group=expansion_direct,colour=expansion_direct))+facet_wrap(.~name,scales="free_y",ncol=5)
-    gg <- gg + geom_point() + geom_line()
-    gg <- gg + labs(y="occurence",colour="",title=paste("les occurences -",id))
-    ggfile <- paste("output/occurence_especes",id,".png",sep="")
-    cat("\n  -> [PNG]",ggfile,"\n")
-    ggsave(ggfile,gg,width=16,height=8)
-
-    if("wide" %in% c(save,output)) {
-        library(reshape2)
-        dout_wide <- dcast(d[,c("num_site_txt","expansion_direct","espece","year","nb_contacts_strict")],num_site_txt+expansion_direct+espece~year)
-        dout_wide <- dout_wide[order(dout_wide$num_site_txt,dout_wide$espece,dout_wide$expansion_direct),]
-    }
-
-    if("wide" %in% c(save)) {
-
-        filecsv <- paste("output/data_abondance_site_year_WIDE_",id,".csv",sep="")
-        cat("\n   --> [CSV]",filecsv)
-        write.table(dout_wide,filecsv,sep="\t",dec=".",row.names=FALSE,quote=TRUE)
-        cat("   DONE !\n")
-
-    }
-
-
-    if("length" %in% c(save)) {
-
-        filecsv <- paste("output/data_abondance_site_year_LENGTH_",id,".csv",sep="")
-        cat("\n   --> [CSV]",filecsv)
-        write.table(d,filecsv,sep="\t",dec=".",row.names=FALSE,quote=TRUE)
-        cat("   DONE !\n")
-
-    }
-
-
-
-
-    if(output == "wide")  return(dout_wide)
-
-    if(output == "length")   return(d)
-
-}
-
-
-
-
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param id
+##' @param donneesAll
+##' @param donneesName
+##' @param method
+##' @param only_direct
+##' @param only_exp
+##' @param seuilOccu
+##' @param col_sp
+##' @param col_date_julien
+##' @param col_site
+##' @param col_nbcontact
+##' @param assessIC
+##' @param listSp
+##' @param tabsp
+##' @param first_year
+##' @param last_year
+##' @param figure
+##' @param description
+##' @param tendanceSurFigure
+##' @param tendanceGroupSpe
+##' @param seuilSignif
+##' @param seuilAbond
+##' @param ecritureStepByStep
+##' @param doBeep
+##' @return
+##' @author Romain Lorrilliere
 main.glm <- function(id=NULL,
                      donneesAll=list("data/data_vigieChiro_DataRP_SpTron_90_site_55sp_withAbs.csv","data/data_vigieChiro_DataRP_SpTron_50_site_55sp_withAbs.csv"),
                      donneesName=c("90","50"),method="glmmTMB",
